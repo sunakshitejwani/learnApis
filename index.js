3340a14d675199ec5737a709f8b4cd167573eb80
sunakshitejwani <sunakshitejwani@gmail.com>
2020-09-25 16:03:35 +0530

    get response with find

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/',(req,res) => {
-    //to get excess to all the props expressjs.com
-    res.send("hello world")
+const courses = [
+  { id: 1, name: "c1" },
+  { id: 2, name: "c2" },
+  { id: 3, name: "c3" }
+];
+
+app.get("/", (req, res) => {
+  //to get excess to all the props expressjs.com
+  res.send("hello world");
 });
 
-app.get('/api/courses', (req,res)=>{
-    res.send([1,2,3]);
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
 });
 
-app.get('/api/posts/:year/:month', (req,res)=>{
-    res.send(req.query);
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(404).send("The course with given id was not found!");
+  }
+  res.send(course);
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
+  console.log(`Listening on port ${port}`);
 });
 //app.post();
 //app.put();
