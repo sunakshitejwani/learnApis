1e859ac4219628f4b3fc27e63ad46834fdad0864
sunakshitejwani <sunakshitejwani@gmail.com>
2020-09-25 15:42:48 +0530

    learn get method

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.get();
-app.post();
-app.put();
-app.delete();
+app.get('/',(req,res) => {
+    //to get excess to all the props expressjs.com
+    res.send("hello world")
+});
+
+app.get('/api/courses', (req,res)=>{
+    res.send([1,2,3]);
+})
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+});
+//app.post();
+//app.put();
+//app.delete();
