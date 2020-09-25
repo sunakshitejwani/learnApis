69528f669a9e35e34a1302193afea5dc3ad59223
sunakshitejwani <sunakshitejwani@gmail.com>
2020-09-25 15:56:54 +0530

    get response with sortby query

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ app.get('/',(req,res) => {
 
 app.get('/api/courses', (req,res)=>{
     res.send([1,2,3]);
-})
+});
+
+app.get('/api/posts/:year/:month', (req,res)=>{
+    res.send(req.query);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
